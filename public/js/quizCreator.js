178b38cf03a8998ba88897a78312d58e73685f20
ParryZChic <supatcha1@hotmail.com>
2020-02-28 01:20:23 +0700

    fetch question when click 'add anouther question'

--- public/js/quizCreator.js
+++ public/js/quizCreator.js
@@ -1,15 +1,16 @@
 var socket = io();
-var questionNum = 0; 
+var questionNum = 0;
+var preQuestionTable = '';
 
-function updateDatabase(){
+function updateDatabase() {
     var questions = [];
     var name = document.getElementById('name').value;
-    for(var i = 1; i <= questionNum; i++){
+    for (var i = 1; i <= questionNum; i++) {
         var question = document.getElementById('q' + i).value;
         var answers = [];
         var type = "";
-        switch(document.getElementById('type'+i).innerText){
-            case("4c"):
+        switch (document.getElementById('type' + i).innerText) {
+            case ("4c"):
                 var answer1 = document.getElementById(i + 'a1').value;
                 var answer2 = document.getElementById(i + 'a2').value;
                 var answer3 = document.getElementById(i + 'a3').value;
@@ -17,26 +18,97 @@ function updateDatabase(){
                 answers = [answer1, answer2, answer3, answer4];
                 type = "4c";
                 break;
-            case("2c"):
+            case ("2c"):
                 var answer1 = document.getElementById(i + 'a1').value;
                 var answer2 = document.getElementById(i + 'a2').value;
                 answers = [answer1, answer2];
                 type = "2c";
                 break;
-            case("sa"):
+            case ("sa"):
                 type = "sa";
-                
+
         }
         var correct = document.getElementById('correct' + i).value;
-        questions.push({"question": question, "answers": answers, "correct": correct, "type":type})
+        questions.push({ "question": question, "answers": answers, "correct": correct, "type": type })
     }
-    
-    var quiz = {id: 0, "name": name, "questions": questions};
+
+    var quiz = { id: 0, "name": name, "questions": questions };
     socket.emit('newQuiz', quiz);
 };
 
+function fetchQuestion() {
+    {
+        console.log(questionNum);
+        var question = document.getElementById('q' + questionNum).value;
+        var answer1 = document.getElementById(questionNum + 'a1').value;
+        var answer2 = document.getElementById(questionNum + 'a2').value;
+        var answer3 = document.getElementById(questionNum + 'a3').value;
+        var answer4 = document.getElementById(questionNum + 'a4').value;
+        var answer = document.getElementById('correct' + questionNum).value;
+        // questionTable = document.getElementById('allQuestions').innerHTML;
+        questionTable = '';
+        
+        questionTable = preQuestionTable + `
+        <h3>Question ${questionNum} :</h3>
+    
+          <div class="tab">
+            <button class="tablinks${questionNum}" onclick="openTab(event, '4 choices${questionNum}', ${questionNum})">Select 4 choices</button>
+            <button class="tablinks${questionNum}" onclick="openTab(event, '2 choices${questionNum}', ${questionNum})">Select 2 choices</button>
+            <button class="tablinks${questionNum}" onclick="openTab(event, 'Short Answer${questionNum}', ${questionNum})">Select Short Answer</button> 
+            <br>
+            <br>
+          </div>
+          
+          <div id="4 choices${questionNum}" class="tabcontent${questionNum}"><br>
+            <label>Question : </label>
+            <label class = "question" id = "q${questionNum}" type = "text" autofocus/>${question}</label>
+            <br>
+            <br>
+            <label>Answer 1: </label>
+            <label id = "${questionNum}a1" type = "text" autofocus/>${answer1}</label>
+            <br><br>
+            <label>Answer 2: </label>
+            <label id = "${questionNum}a1" type = "text" autofocus/>${answer2}</label>
+            <br><br>
+            <label>Answer 3: </label>
+            <label id = "${questionNum}a1" type = "text" autofocus/>${answer3}</label>
+            <br><br>
+            <label>Answer 4: </label>
+            <label id = "${questionNum}a1" type = "text" autofocus/>${answer4}</label>
+            <br>
+            <br>
+            <label>Correct Answer (1-4) :</label>
+            <label class = "correct" id = "correct${questionNum}"  type = "number" autofocus/>${answer}</label>
+            <label id = "${questionNum}a1" type = "text" autofocus/></label>
+        </div>
+          <div id="2 choices${questionNum}" class="tabcontent${questionNum}" style="display:none"><br>
+            <label>Question : </label>
+            <label class = "question" id = "q${questionNum}" type = "text" autofocus/>${question}</label>
+            <br>
+            <br>
+            <label>Correct Answer (1-2) :</label>
+            <label class = "correct" id = "correct${questionNum}"  type = "number" autofocus/>${answer}</label>
+          </div>
+          
+          <div id="Short Answer${questionNum}" class="tabcontent${questionNum}" style="display:none"><br>
+            <label>Question : </label>
+            <label class = "question" id = "q${questionNum}" type = "text" autofocus/>${question}</label>
+            <br>
+            <br>
+            <label>Correct Answer :</label>
+            <label class = "correct" id = "correct${questionNum}"  type = "number" autofocus/>${answer}</label>
+            <br>
+            <br>
+          </div>
+        <br>`;
+        preQuestionTable = questionTable;
+        document.getElementById('allQuestions').innerHTML = questionTable;
+    }
+}
+
 var questionTable = "";
-function addQuestion(){
+function addQuestion() {
+    if (questionNum >= 1) fetchQuestion();
     questionNum += 1;
     questionTable = document.getElementById('allQuestions').innerHTML;
     questionTable += `
@@ -49,20 +121,20 @@ function addQuestion(){
         <br>
         <br>
       </div>
-      
-      <div id="4 choices${questionNum}" class="tabcontent${questionNum}"><br>
-        <label>Question : </label>
+      <br><br>
+      <label>Question : </label>
         <input class = "question" id = "q${questionNum}" type = "text" autofocus/>
-        <br>
+      <div id="4 choices${questionNum}" class="tabcontent${questionNum}"><br>
         <br>
         <label>Answer 1: </label>
         <input id = "${questionNum}a1" type = "text" autofocus/>
+        <br><br>
         <label>Answer 2: </label>
         <input id = "${questionNum}a2" type = "text" autofocus/>
-        <br>
-        <br>
+        <br><br>
         <label>Answer 3: </label>
         <input id = "${questionNum}a3"  type = "text"autofocus/>
+        <br><br>
         <label>Answer 4: </label>
         <input id = "${questionNum}a4"  type = "text" autofocus/>
         <br>
@@ -72,24 +144,14 @@ function addQuestion(){
       </div>
       
       <div id="2 choices${questionNum}" class="tabcontent${questionNum}" style="display:none"><br>
-        <label>Question : </label>
-        <input class = "question" id = "q${questionNum}" type = "text" autofocus/>
-        <br>
-        <br>
-        <label>Answer 1: </label>
-        <input id = "${questionNum}a1" type = "text" autofocus/>
-        <label>Answer 2: </label>
-        <input id = "${questionNum}a2" type = "text" autofocus/>
-        <br>
+
         <br>
         <label>Correct Answer (1-2) :</label>
         <input class = "correct" id = "correct${questionNum}"  type = "number" autofocus/>
       </div>
       
       <div id="Short Answer${questionNum}" class="tabcontent${questionNum}" style="display:none"><br>
-        <label>Question : </label>
-        <input class = "question" id = "q${questionNum}" type = "text" autofocus/>
-        <br>
+        
         <br>
         <label>Correct Answer :</label>
         <input class = "correct" id = "correct${questionNum}" type = "text" autofocus/>
@@ -100,60 +162,60 @@ function addQuestion(){
     document.getElementById('allQuestions').innerHTML = questionTable;
 }
 
-function openTab(evt, quizType,id) {
+function openTab(evt, quizType, id) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName(`tabcontent${id}`);
     for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+        tabcontent[i].style.display = "none";
     }
     tablinks = document.getElementsByClassName(`tablinks${id}`);
     for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
     document.getElementById(quizType).style.display = "block";
     evt.currentTarget.className += " active";
 }
 
-function addQuestion4c(){
+function addQuestion4c() {
     questionNum += 1;
-    
+
     var questionsDiv = document.getElementById('allQuestions');
     var newQuestionDiv = document.createElement("div");
     var questionType = document.createElement("div");
-    
+
     var questionLabel = document.createElement('label');
     var questionField = document.createElement('input');
-    
+
     var answer1Label = document.createElement('label');
     var answer1Field = document.createElement('input');
-    
+
     var answer2Label = document.createElement('label');
     var answer2Field = document.createElement('input');
-    
+
     var answer3Label = document.createElement('label');
     var answer3Field = document.createElement('input');
-    
+
     var answer4Label = document.createElement('label');
     var answer4Field = document.createElement('input');
-    
+
     var correctLabel = document.createElement('label');
     var correctField = document.createElement('input');
 
     questionType.innerHTML = "4c";
-    questionType.setAttribute('id','type'+String(questionNum));
+    questionType.setAttribute('id', 'type' + String(questionNum));
     questionType.setAttribute('style', 'display:none');
 
     questionLabel.innerHTML = "Question " + String(questionNum) + ": ";
     questionField.setAttribute('class', 'question');
     questionField.setAttribute('id', 'q' + String(questionNum));
     questionField.setAttribute('type', 'text');
-    
+
     answer1Label.innerHTML = "Answer 1: ";
     answer2Label.innerHTML = " Answer 2: ";
     answer3Label.innerHTML = "Answer 3: ";
     answer4Label.innerHTML = " Answer 4: ";
     correctLabel.innerHTML = "Correct Answer (1-4): ";
-    
+
     answer1Field.setAttribute('id', String(questionNum) + "a1");
     answer1Field.setAttribute('type', 'text');
     answer2Field.setAttribute('id', String(questionNum) + "a2");
@@ -164,7 +226,7 @@ function addQuestion4c(){
     answer4Field.setAttribute('type', 'text');
     correctField.setAttribute('id', 'correct' + String(questionNum));
     correctField.setAttribute('type', 'number');
-    
+
     newQuestionDiv.setAttribute('id', 'question-field');//Sets class of div
 
     newQuestionDiv.appendChild(questionType);
@@ -187,51 +249,51 @@ function addQuestion4c(){
     newQuestionDiv.appendChild(document.createElement('br'));
     newQuestionDiv.appendChild(correctLabel);
     newQuestionDiv.appendChild(correctField);
-    
+
     questionsDiv.appendChild(document.createElement('br'));//Creates a break between each question
     questionsDiv.appendChild(newQuestionDiv);//Adds the question div to the screen
-    
+
     newQuestionDiv.style.backgroundColor = randomColor();
 }
-function addQuestion2c(){
+function addQuestion2c() {
     questionNum += 1;
-    
+
     var questionsDiv = document.getElementById('allQuestions');
     var newQuestionDiv = document.createElement("div");
     var questionType = document.createElement("div");
-    
+
     var questionLabel = document.createElement('label');
     var questionField = document.createElement('input');
-    
+
     var answer1Label = document.createElement('label');
     var answer1Field = document.createElement('input');
-    
+
     var answer2Label = document.createElement('label');
     var answer2Field = document.createElement('input');
-    
+
     var correctLabel = document.createElement('label');
     var correctField = document.createElement('input');
 
     questionType.innerHTML = "2c";
-    questionType.setAttribute('id','type'+String(questionNum));
+    questionType.setAttribute('id', 'type' + String(questionNum));
     questionType.setAttribute('style', 'display:none');
 
     questionLabel.innerHTML = "Question " + String(questionNum) + ": ";
     questionField.setAttribute('class', 'question');
     questionField.setAttribute('id', 'q' + String(questionNum));
     questionField.setAttribute('type', 'text');
-    
+
     answer1Label.innerHTML = "Answer 1: ";
     answer2Label.innerHTML = " Answer 2: ";
     correctLabel.innerHTML = "Correct Answer (1-2): ";
-    
+
     answer1Field.setAttribute('id', String(questionNum) + "a1");
     answer1Field.setAttribute('type', 'text');
     answer2Field.setAttribute('id', String(questionNum) + "a2");
     answer2Field.setAttribute('type', 'text');
     correctField.setAttribute('id', 'correct' + String(questionNum));
     correctField.setAttribute('type', 'number');
-    
+
     newQuestionDiv.setAttribute('id', 'question-field');//Sets class of div
 
     newQuestionDiv.appendChild(questionType);
@@ -248,41 +310,41 @@ function addQuestion2c(){
     newQuestionDiv.appendChild(document.createElement('br'));
     newQuestionDiv.appendChild(correctLabel);
     newQuestionDiv.appendChild(correctField);
-    
+
     questionsDiv.appendChild(document.createElement('br'));//Creates a break between each question
     questionsDiv.appendChild(newQuestionDiv);//Adds the question div to the screen
-    
+
     newQuestionDiv.style.backgroundColor = randomColor();
 }
-function addQuestionSA(){
+function addQuestionSA() {
     questionNum += 1;
-    
+
     var questionsDiv = document.getElementById('allQuestions');
     var newQuestionDiv = document.createElement("div");
     var questionType = document.createElement("div");
-    
+
     var questionLabel = document.createElement('label');
     var questionField = document.createElement('input');
-    
+
     var correctLabel = document.createElement('label');
     var correctField = document.createElement('input');
 
     questionType.innerHTML = "sa";
-    questionType.setAttribute('id','type'+String(questionNum));
+    questionType.setAttribute('id', 'type' + String(questionNum));
     questionType.setAttribute('style', 'display:none');
-    
+
     questionLabel.innerHTML = "Question " + String(questionNum) + ": ";
     questionField.setAttribute('class', 'question');
     questionField.setAttribute('id', 'q' + String(questionNum));
     questionField.setAttribute('type', 'text');
-    
+
     correctLabel.innerHTML = "Correct Answer: ";
-    
+
     correctField.setAttribute('id', 'correct' + String(questionNum));
     correctField.setAttribute('type', 'text');
-    
+
     newQuestionDiv.setAttribute('id', 'question-field');//Sets class of div
-    
+
     newQuestionDiv.appendChild(questionType);
     newQuestionDiv.appendChild(document.createElement('br'));
     newQuestionDiv.appendChild(questionLabel);
@@ -291,31 +353,31 @@ function addQuestionSA(){
     newQuestionDiv.appendChild(document.createElement('br'));
     newQuestionDiv.appendChild(correctLabel);
     newQuestionDiv.appendChild(correctField);
-    
+
     questionsDiv.appendChild(document.createElement('br'));//Creates a break between each question
     questionsDiv.appendChild(newQuestionDiv);//Adds the question div to the screen
-    
+
     newQuestionDiv.style.backgroundColor = randomColor();
 }
 //Called when user wants to exit quiz creator
-function cancelQuiz(){
+function cancelQuiz() {
     if (confirm("Are you sure you want to exit? All work will be DELETED!")) {
         window.location.href = "../";
     }
 }
 
-socket.on('startGameFromCreator', function(data){
+socket.on('startGameFromCreator', function (data) {
     window.location.href = "../../host/?id=" + data;
 });
 
-function randomColor(){
-    
+function randomColor() {
+
     var colors = ['#4CAF50', '#f94a1e', '#3399ff', '#ff9933'];
     var randomNum = Math.floor(Math.random() * 4);
     return colors[randomNum];
 }
 
-function setBGColor(){
+function setBGColor() {
     var randColor = randomColor();
     document.getElementById('question-field').style.backgroundColor = randColor;
 }

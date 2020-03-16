331e82609b7a11f15582935b8633b89ea180ce8d
Bright <pongpuk>
2020-03-16 17:32:22 +0700

    complete homework

--- public/js/course.js
+++ public/js/course.js
@@ -2,11 +2,12 @@ var socket = io();
 
 socket.on('connect', function(){
     socket.emit('requestDbHW');//Get database names to display to user
+    socket.emit('requestDbNames');
 });
 
 socket.on('HWData', function(data){
     for(var i = 0; i < Object.keys(data).length; i++){
-        var div = document.getElementById('game-list');
+        var div = document.getElementById('hw-list');
         var button = document.createElement('button');
         var editbutton = document.createElement('button');
         
@@ -17,7 +18,28 @@ socket.on('HWData', function(data){
         editbutton.innerText = "edit";
         editbutton.setAttribute('onClick', "edit('"+data[i].id+"')");
         editbutton.setAttribute('id', "gameButton");
+        editbutton.setAttribute('class',"editButton");
+        div.appendChild(button);
+        div.appendChild(editbutton);
+        div.appendChild(document.createElement('br'));
+        div.appendChild(document.createElement('br'));
+    }
+});
+
+socket.on('gameNamesData', function(data){
+    for(var i = 0; i < Object.keys(data).length; i++){
+        var div = document.getElementById('quiz-list');
+        var button = document.createElement('button');
+        var editbutton = document.createElement('button');
         
+        button.innerHTML = data[i].name;
+        button.setAttribute('onClick', "startGame('" + data[i].id + "')");
+        button.setAttribute('id', 'gameButton');
+        
+        editbutton.innerText = "edit";
+        editbutton.setAttribute('onClick', "edit('"+data[i].id+"')");
+        editbutton.setAttribute('id', "gameButton");
+        editbutton.setAttribute('class',"editButton");
         div.appendChild(button);
         div.appendChild(editbutton);
         div.appendChild(document.createElement('br'));
